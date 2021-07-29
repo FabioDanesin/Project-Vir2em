9eff376acabd4f4cdc3c3ff8b5b9952e573be4e3
fabio <f.danesin99@gmail.com>
2021-07-29 14:35:34 +0200

    commentato meglio

--- Client.py
+++ Client.py
@@ -5,8 +5,14 @@ import Server
 URL = Server.URL
 NAME = Server.NAME
 NAMESPACE = Server.NAMESPACE
-
+# da ricordare ma da eliminare
 def make_client():
     client = Client(URL)
-    client.register_namespace(Server.NAMESPACE)
+    client.connect()
+    print("connected")
+    nodes = client.get_namespace_array()
+    print(nodes)
+    omron_server = str(nodes[2])
 
+if __name__ == '__main__':
+    make_client()
--- Server.py
+++ Server.py
@@ -1,28 +1,34 @@
 from opcua import Server, Node
 import time
 
-URL = "opc.tcp://192.168.1.5:4851"
-NAME = "testserver"
-NAMESPACE = "testnamespace"
+URL = 'opc.tcp://157.138.24.165:4840'
+NAME = 'testserver'
+NAMESPACE = 'testnamespace'
 
 
 def setup():
-    # apre il server su un dato URL definito dal PLC
-    server = Server(URL)
-    server.set_endpoint(URL) # setta il punto di ascolto
-    server.set_server_name(NAME)# setta il nome del server
 
+    # istanzia la classe server
+    server = Server()
+    print("starting")
+    server.set_endpoint(URL)  # setta il punto di ascolto
+    server.set_server_name(NAME)  # setta il nome del server
+
+    # Registra un namespace. Un namespace è un container per i nodes del server
+    # Il valore ritornato è il numero dei namespace registrati
+    # Doc: https://reference.opcfoundation.org/v104/Core/docs/Part3/8.2.2/
     space = server.register_namespace(NAMESPACE)
+    # Estrae il primo Node per gli oggetti. Un Node può essere visto come un container per le variabili del namespace
     node = server.get_objects_node()
 
-    param: Node = node.add_object(space, "Parameters")
-
+    param = node.add_object(space, "Parameters")
     text = param.add_variable(space, "Text variable", "String of text")
     _int = param.add_variable(space, "Integer variable", 1102)
+    print("starting")
 
     while True:
         print(text.mro)
-        print(_int.mro)
+        print(_int)
         time.sleep(1)
 
 
