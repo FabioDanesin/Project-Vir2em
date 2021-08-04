2e0f7b4a3e556847537c1ba71893523cd2580b7e
fabio <f.danesin99@gmail.com>
2021-08-04 14:32:36 +0200

    Rilavorazione significativa

--- Client.py
+++ Client.py
@@ -6,9 +6,6 @@ ip = "157.138.24.165"
 port = "4840"
 url = "opc.tcp://" + ip + ":" + port
 
-# Istanze della classe monitor. La lunghezza della lista deve essere settata sempre a 1
-__monitorinstances__ = []
-
 
 class __Monitor:
     """
@@ -16,7 +13,7 @@ class __Monitor:
         controller e non chiede richiesta di autenticazione. NOTA CHE QUESTA CLASSE DEVE ESSERE ISTANZIATA UNA SOLA
         VOLTA!
     """
-    __exists__ = False
+    # Istanze della classe monitor. La lunghezza della lista deve essere settata sempre a 1
 
     def __init__(self, update_time: float, _url: str = url, show_on_console: bool = True):
         """
@@ -29,14 +26,16 @@ class __Monitor:
         """
 
         # Funzione per un thread daemon separato, esegue la lettura e il display dei valori contenuti nel controllore
-        # e li stampa a console.
+        # e li stampa a console. Da verificare se serve mantenerlo così o se è possibile usare una lambda
         def polling(sleeptime):
-            for data in self.__parameters__:
-                print("|>" + data.get_browse_name().Name + " : " + str(data.get_value()))
+            for d in self.__variables__:
+                print("|>" + d.get_browse_name().Name + " : " + str(d.get_value()))
             time.sleep(sleeptime)
 
+        self.__monitorinstances__ = []
+
         # Check eseguito per mantenere la proprietà singleton
-        if len(__monitorinstances__) == 1:
+        if len(self.__monitorinstances__) == 1:
             print("Esiste già un monitor")
             raise RuntimeError()
 
@@ -47,8 +46,30 @@ class __Monitor:
         try:
             # Connessione del client sul punto di ascolto definito dall' URL
             self.__client__.connect()
+            self.__obj_node__ = self.__client__.get_objects_node()
+
+            # Estrazione del nome del controller
+            controllername = open("ControllerName.txt", "r").readline(30)
+
+            # Estrazione delle variabili di stato del controller
+            self.__controller_state_variables__ = self.__obj_node__.get_children()
+            self.__variables__ = None
+
             # Ottenimento dei parametri dal controller
-            self.__parameters__ = self.__client__.get_objects_node().get_children()[1].get_children()[1].get_children()
+            for data in self.__controller_state_variables__:
+
+                # Individua il nodo che contiene il nome del controller
+                if data.get_browse_name().Name == controllername:
+
+                    # Lo spazio delle GlobalVars è hardcoded con questo nome
+                    for plcvars in data.get_children():
+                        if plcvars.get_browse_name().Name == "GlobalVars":
+                            self.__variables__ = plcvars.get_children()
+
+            if self.__variables__ is None:
+                print("Errore: controller non trovato. Il nome dato è " + controllername)
+                raise RuntimeError()
+
         except Exception:
 
             # Se a qualsiasi punto dovesse fallire il client si disconnetterà automaticamente
@@ -65,7 +86,7 @@ class __Monitor:
             t.start()
             t.join()
 
-        Client.__monitorinstances__ = [self]  # Proprietà singleton
+        self.__monitorinstances__ = [self]  # Proprietà singleton
 
     # def __init__(self, ip: str, port: str):
     #    self.__init__("opc.tcp://" + ip + port)
@@ -73,42 +94,20 @@ class __Monitor:
     def __str__(self):
         return self.__name__
 
-    @staticmethod
-    def __get_instance__():
+    def __get_instance__(self):
         """
         Metodo esterno per ottenere l'istanza del monitor. Nota che se un istanza monitor ancora non esiste, ritornerà
         None, senza istanziarne una con parametri a sua scelta.
         :return: l'istanza di Monitor se ne esiste una, None altrimenti.
         """
-        if len(__monitorinstances__) == 1:
-            return __monitorinstances__[0]
+        if len(self.__monitorinstances__) == 1 and self.__monitorinstances__[1] is not None:
+            return self.__monitorinstances__[0]
         else:
             return None
 
-
-"""
-def monitor():
-    client = Client(url)
-    client.connect()
-    try:
-        obj_node = client.get_objects_node()
-        children = obj_node.get_children()
-        params = children[1].get_children()[1].get_children()
-        while True:
-            print("---------------------------------------------------------------------------------------------------")
-            for data in params:
-                print("|>" + data.get_browse_name().Name + ":" + str(data.get_value()))
-            print("---------------------------------------------------------------------------------------------------")
-
-            time.sleep(1)
-
-    except Exception:
-        print("Exception thrown")
-        traceback.print_exc()
-
-    finally:
-        client.disconnect()
-"""
+    def __del__(self):
+        self.__monitorinstances__ = []
+        self.__client__.disconnect()
 
 
 def testclassmonitor():
--- ClientPack/DBmanager.py
+++ ClientPack/DBmanager.py
@@ -1,15 +1,14 @@
 ######################################################
 # FIXME: import di psycopg2 fallisce, non so come mai
 ######################################################
-import collections
+from flask_sqlalchemy import *
+from sqlalchemy import MetaData, Table, Column, create_engine
 
-from sqlalchemy import MetaData, Table, Column
-from sqlalchemy.engine import create_engine
 
 pgsql_name = "postgres"  # Nome del login su postgresql
 pgsql_password = "SuperUser"  # Password
-pgsql_ip = "127.0.0.1"  # IP su cui il DB è locato
-pgsql_port = "5432"  # port
+pgsql_ip = "127.0.0.1"      # IP su cui il DB è locato
+pgsql_port = "5432"         # port
 
 db_url = "postgresql://" + pgsql_name + "@" + pgsql_password + pgsql_ip + ":" + pgsql_port
 
@@ -22,17 +21,17 @@ class DBmanager:
     in quanto opera in ORM.
     """
 
-    # Mantenimento della proprietà singleton assicurato tramite questo campo
-    __exists__ = []
-
     def __init__(self, url):
+        # Mantenimento della proprietà singleton assicurato tramite questo campo
+        self.__exists__ = []
+
         # Check a runtime per il controllo della proprietà singleton.
         # Potrebbe non essere foolproof ma è l'una del mattino quindi ci penso domani
         if len(self.__exists__) == 1 and self.__exists__[0] is not None:
             raise RuntimeError()
 
         # Crea l'engine per comunicare con il DB
-        self.__engine__ = create_engine(url)
+        self.__engine__ = create_engine(url, dialect="postgresql", echo=True)
 
         # Effettua la connessione all'url
         self.__connection__ = self.__engine__.connect()
@@ -41,4 +40,5 @@ class DBmanager:
         self.__exists__ = [self]
 
         # Questa classe serve per mantenere tutti di dati parzialmente parsati prima di "pusharli" sul database
-        self.__metadata__ = MetaData(self.__engine__)
+        self.__metadata__ = MetaData()
+
