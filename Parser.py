d37c3c84bf431278a3c7c59a4c51bd42bd2e3646
fabio <f.danesin99@gmail.com>
2021-08-05 12:21:30 +0200

    Nuovo file

---
+++ Parser.py
@@ -0,0 +1,14 @@
+class Data:
+    def __init__(self, Name="new_Controller_0", Ip="192.168.250.1", Port="4880") -> None:
+        self.__Name__: str = Name
+        self.__ip__: str = Ip
+        self.__port__: str = Port
+        self.__DBname__: str
+        self.__DBpsswd__: str
+        self.__DBip__: str
+        self.__DBport__: str
+
+
+
+
+
