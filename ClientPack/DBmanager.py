596f865f6ebbe3b3a2bbd1e7bd59daa80073bc28
fabio <f.danesin99@gmail.com>
2021-09-02 20:49:03 +0200

    Aggiunto hashing delle password e nomi e differenziazione del polling del PLC

--- ClientPack/DBmanager.py
+++ ClientPack/DBmanager.py
@@ -1,34 +1,46 @@
-import timeit
-
 from sqlalchemy import MetaData, create_engine, inspect, Table
 from sqlalchemy.sql import select
 from sqlalchemy.ext.automap import automap_base
-from Logger import Logger, Filetype
 
-import threading
+from Control import KeyNames
+from Control.Parser import __Data__
+from Control.Logger import Logger, Filetype
+
 from time import sleep, perf_counter
 
-db_type = "postgresql"
-db_dialect = "psycopg2"
-user_name = "fabio"             # Nome del login su postgresql, deve anche essere il nome del database
-user_password = "Vir2em_Fabio"  # Password
-ip = "localhost"                # IP su cui il DB è locato
-port = "5432"
+import threading
+import hashlib
+
+parsed_data = __Data__.Data()
+
+
+def get_from_parsed_data(name):
+    return parsed_data.get(name)
+
+
+db_type = get_from_parsed_data(KeyNames.db_type)
+user_name = get_from_parsed_data(KeyNames.db_admin_name)          # Nome del login su postgresql, deve anche essere il nome del database
+user_password = get_from_parsed_data(KeyNames.db_admin_password)  # Password
+ip = get_from_parsed_data(KeyNames.ip)                            # IP su cui il DB è locato
+port = get_from_parsed_data(KeyNames.port)
 
 # URL completo. L'ordine dei parametri non deve essere cambiato
-url = db_type + "+" + db_dialect + "://" + user_name + ":" + user_password + "@" + ip + ":" + port + "/"
+database_url = db_type + "psycopg2 ://" + user_name + ":" + user_password + "@" + ip + ":" + port + "/"
 
-_log_path_ = ""      # TODO: Implementare logging tramite parser
+_log_path_ = parsed_data.get(KeyNames.logs)
 
 
 # TODO : debug only
 def makedb():
-    eng = create_engine(url)
+    eng = create_engine(database_url)
     con = eng.connect()
     inspector = inspect(eng)
     base = automap_base()
     base.prepare(eng, reflect=True)
     return eng, con, inspector, eng, MetaData()
+
+
+instance = None
 
 
 class DBmanager:
@@ -38,32 +50,33 @@ class DBmanager:
     account già collegato da cui poi opera. Non è necessario imparare la sintassi del database per usare questa classe
     in quanto opera in ORM.
     """
-    # Proprietà singleton statica
-    __exists__ = None
 
-    def __init__(self, datbase_url, auto_update=True, poll=3):
-        def poller(polltime):
+    def __init__(self, auto_update: bool = True, sleeptime: float = 3.):
+
+        def poll():
+            for name in self.__get_all_table_names__():
+                timer = perf_counter()
+                self.__update_table__(name)
+                elapsed = perf_counter() - timer
+                self.__logger__.write(f"Aggiornata tabella {name} in {elapsed:0.3f} secondi")
+
+        def poll_on_timer(polltime):
 
             while True:
                 sleep(polltime)
                 for name in self.__get_all_table_names__():
-                    timer = perf_counter()
-                    self.__update_table__(name)
-                    elapsed = perf_counter() - timer
-                    self.__logger__.write(f"Aggiornata tabella {name} in {elapsed:0.3f} secondi")
-        # Singleton
-        if self.__exists__ is not None:
-            raise RuntimeError()
+                    poll()
+
         self.__logger__ = Logger(_log_path_, "Database log", Filetype.LOCAL)
 
         # Crea l'engine per comunicare con il DB
-        self.__engine__ = create_engine(datbase_url)
+        self.__engine__ = create_engine(database_url)
 
         # Inspector espone dei metodi per l'ottenimento delle tabelle presenti nel database in modo da avere una visione
         # piena dell'intero schema. Richiede un engine a cui eseguire il binding.
         self.__inspector__ = inspect(self.__engine__)
 
-        # Effettua la connessione all'url
+        # Effettua la connessione all'database_url
         self.__connection__ = self.__engine__.connect()
 
         # Questa classe serve per mantenere tutti di dati parzialmente parsati prima di "pusharli" sul database
@@ -76,22 +89,32 @@ class DBmanager:
         self.__get_all_rows_in_db__()
         self.__logger__.write("Sincronizzazione avvenuta con successo")
 
-        # Proprietà singleton
-        self.__exists__ = self
-
         if auto_update:
             t = threading.Thread(
                 name="Database update",
-                target=poller(poll),
+                target=poll_on_timer(sleeptime),
+                daemon=True
+            )
+        else:
+            t = threading.Thread(
+                name="Database update",
+                target=poll(),
                 daemon=True
             )
 
     def check_credentials(self, name, password):
-        # Copia senza eliminare la tabella users
+        def hash_str(s: str):
+            return hashlib.sha256(s.encode()).hexdigest()
+
+        # Copia i contenuti la tabella users
         users = self.__tables__.get('users')
+
+        hashedpass = hash_str(password)
+        hashedname = hash_str(name)
+
         # Compara le triple
         for data in users:
-            if (name, password) == (data[1], data[2]):
+            if (hashedpass, hashedname) == (data[1], data[2]):
                 return True
         return False
 
@@ -106,8 +129,8 @@ class DBmanager:
         for name in self.__get_all_table_names__():
             tablenames.append(
                 Table(
-                    name,                          # Nome della tabella
-                    self.__metadata__,             # Contenitore dei metadati della tabella
+                    name,  # Nome della tabella
+                    self.__metadata__,  # Contenitore dei metadati della tabella
                     autoload_with=self.__engine__  # Importante, dice al database che la tabella è già presente
                 )
             )  # Preleva gli attributi dalle tabelle
