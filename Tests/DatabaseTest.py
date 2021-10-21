3bc890ccd1b797517cc30fca2d2aa4d2df052c23
fabio <f.danesin99@gmail.com>
2021-10-21 10:46:11 +0200

    Aggiunto test

---
+++ Tests/DatabaseTest.py
@@ -0,0 +1,20 @@
+from Configuration.DBmanager import DBmanager, SqlDataNotFoundError
+
+import time
+begin_time = time.time()
+
+manager = DBmanager()
+
+name = "Vir2em_Fabio"
+password = "linuxmanager"
+
+manager.check_credentials(name, password)
+try:
+    manager.__query_table__('testtable')
+except SqlDataNotFoundError as s:
+    print(s)
+
+r = manager.__query_table__('users')
+print(f"r = {r}")
+
+print(f"Elapsed = {time.time() - begin_time}")
