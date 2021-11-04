5e58900bf70a04e604f2b7855fdadbdeeaec3827
fabio <f.danesin99@gmail.com>
2021-11-04 10:22:47 +0100

    GET/POST testing

--- Frontend/MainPage.py
+++ Frontend/MainPage.py
@@ -6,15 +6,17 @@ app.template_folder = "Templates"
 
 @app.route("/otherpage", methods=['POST'])
 def other():
-    x = 'post test'
-    return render_template("otherpage.html", test=x)
+    return render_template("otherpage.html")
 
 
-@app.route("/mainpage", methods=['GET', 'POST'])
+@app.route("/mainpage", methods=['POST'])
+def main_page_post():
+    d = request.form['text']
+    return d
+
+
+@app.route("/mainpage")
 def main_page():
-    if request.method == 'POST':
-        print("Posted")
-        return redirect("/otherpage")
     return render_template("mainpage.html", test='posttest')
 
 
