891e87ac3c2b9313d346990e600a0bc3e735eb6f
fabio <f.danesin99@gmail.com>
2021-11-25 14:40:08 +0100

    Aggiornato con login attempt funzionante

--- Frontend/MainPage.py
+++ Frontend/MainPage.py
@@ -1,39 +1,58 @@
 import requests
-from Configuration.DBmanager import DBmanager as Database, SqlDataNotFoundError
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, make_response
+import os
+import pathlib
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_required
+from Configuration.DBmanager import DBmanager, SqlDataNotFoundError
 
+"""
+        Utilities
+"""
 
-db = Database.get_instance()
+db = DBmanager.get_instance()
+
+"""
+        Configurazione Flask App
+"""
+
 app = Flask(__name__)
-app.template_folder = "Templates"
+app.debug = True
+templatedir = os.path.join(pathlib.Path(__file__).parent.resolve(), "templates")
+app.template_folder = templatedir
+app.config['SECRET_KEY'] = "ASDASFCVERV2934282374"
+app.config['ENV'] = "development"
+
+bycrypt = Bcrypt(app)
+
+loginmanager = LoginManager(app)
+loginmanager.login_view = "loginpage"
+loginmanager.login_message_category = "info"
+
+"""
+                    Routes
+"""
 
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/login")
+@loginmanager.user_loader
+def load_user():
     error = False
+    reason = ""
+
     if request.method == 'POST':
         uname = request.form['username']
         pw = request.form['password']
         try:
             db.check_credentials(uname, pw)
-            return redirect("/data")
-        except SqlDataNotFoundError:
-            error=True
-    return render_template("login.html", error=error)
+        except SqlDataNotFoundError as sql:
+            error = True
+            reason = sql.__str__()
+            print(reason)
 
-@app.route("/")
-@app.route("/mainpage", methods=['GET', 'POST'])
-def main_page():
-    test = 'posttest'
-    print(url_for("/otherpage"))
-    if request.method == 'POST':
-        test = request.form['text']
-
-        if test == 'spurdo':
-            test = requests.post(url=url_for("/otherpage"))
-
-    return render_template("mainpage.html", test=test)
+    return render_template("loginpage.html", error=error, reason=reason)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='localhost')
