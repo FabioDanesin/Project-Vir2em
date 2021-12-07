02a42e22b7c97fb68aa92b05398c35734c75db94
fabio <f.danesin99@gmail.com>
2021-12-07 10:16:00 +0100

    Gettate fondamenta autenticazione SCRAM

--- Frontend/MainPage.py
+++ Frontend/MainPage.py
@@ -1,38 +1,93 @@
+import json
 import os
 import pathlib
+from random import Random
+
 import requests
+
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_required, current_user
 from Configuration.DBmanager import DBmanager, SqlDataNotFoundError
-from flask import Flask, redirect, request, render_template, url_for, make_response
+from flask import Flask, redirect, request, render_template, url_for, make_response, Response
+from scramp import ScramMechanism, ScramClient, ScramException
 
 authenticated = False
+TEMPLATE_DIR = "templates"
+DEFAULT_NONCE_LENGTH = 128
 
-"""
-        Utilities
-"""
+#
+# Utilities
+#
 
 db = DBmanager.get_instance()
 
-"""
-        Configurazione Flask App
-"""
+
+def generate_nonce(length=DEFAULT_NONCE_LENGTH):
+    random = Random()
+    s = ""
+
+    for a in range(length):
+        i = random.randint(33, 126)  # Caratteri contemplati da UTF-8 che non sono caratteri speciali.
+        charachter = chr(i)
+        s = s + charachter
+
+    return s
+
+
+#
+# Configurazione Flask App
+#
+
+HOST = "localhost"
 
 app = Flask(__name__)
 app.debug = True
-templatedir = os.path.join(pathlib.Path(__file__).parent.resolve(), "templates")
+templatedir = os.path.join(pathlib.Path(__file__).parent.resolve(), TEMPLATE_DIR)
 app.template_folder = templatedir
 app.config['SECRET_KEY'] = "ASDASFCVERV2934282374"
 app.config['ENV'] = "development"
 
 bycrypt = Bcrypt(app)
 
-"""login_manager = LoginManager(app)
-login_manager.login_view = 'datapage'"""
 
-"""
-                    Routes
-"""
+#
+# Routes
+#
+
+# TODO: da escludere GET
+@app.route("/datarequest", methods=["GET", "POST"])
+def get_data():
+    if request.method == "POST":
+        body = request.json
+        print(body)
+        response = requests.Response()
+        response.status_code = 200
+
+        return Response(
+            status=200,
+            response=json.dumps(
+                {
+                    "Name": "Sensor",
+                    "Value": 21
+                }
+            ),
+            content_type="application/json"
+        )
+
+
+@app.route("/sendrequest", methods=["GET"])
+def request_url():
+    datadict = {
+        "Dataname": "NAME",
+        "Datavalue": "231"
+    }
+    r = requests.post(
+        "http://127.0.0.1:5000",
+        json=datadict
+    )
+    print(r)
+    print(str(r.cookies))
+    return "success"
 
 
 @app.route("/data", methods=['GET', 'POST'])
@@ -46,29 +101,11 @@ def datapage():
     return render_template("mainpage.html", test=error)
 
 
-"""
-
-@app.route("/", methods=['GET', 'POST'])
-@app.route("/login")
-@login_manager.user_loader
-def loadser():
-    error = False
-    reason = ""
-
-    if request.method == 'POST':
-        uname = request.form['username']
-        pw = request.form['password']
-        try:
-            db.check_credentials(uname, pw)
-            return redirect(url_for("datapage"))
-        except SqlDataNotFoundError as sql:
-            error = True
-            reason = sql.__str__()
-            print(reason)
-
-    return render_template("loginpage.html", error=error, reason=reason)
-
-"""
+@app.route("/logout")
+def logout():
+    global authenticated
+    authenticated = False
+    redirect(url_for("load_user"))
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -82,8 +119,9 @@ def load_user():
     if request.method == "POST":
         try:
 
-            db.check_credentials(request.form['username'], request.form['password'])
-            authenticated = not authenticated
+            # db.check_credentials(request.form['username'])
+            db.check_credentials("Vir2em_Fabio")
+            authenticated = True
             return redirect(url_for("datapage"))
         except SqlDataNotFoundError as s:
             error = True
@@ -93,4 +131,4 @@ def load_user():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
+    app.run(debug=True, host=HOST)
