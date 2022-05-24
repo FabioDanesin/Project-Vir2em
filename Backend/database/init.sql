5cd32772f3d2eed147bddd4a876495de1e120192
FabioDanesin <f.danesin99@gmail.com>
2022-05-24 14:03:43 +0200

    Aggiunto file di setup per database docker

---
+++ Backend/database/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS data
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE IF NOT EXISTS users
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
