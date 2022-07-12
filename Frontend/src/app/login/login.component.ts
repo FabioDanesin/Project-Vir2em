40e936b156f3507e7557f7c7f6b4f119dc26352a
FabioDanesin <f.danesin99@gmail.com>
2022-07-12 13:02:56 +0200

    Implemenato login funzionante.

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -4,7 +4,8 @@ import { Router } from '@angular/router';
 import { Globals } from 'src/globals';
 import { AppRoutingModule } from '../app-routing.module';
 import { AppComponent } from '../app.component';
-const crypto  = require('crypto-js');
+var crypto = require("crypto-js");
+
 /*
 @NgModule({
 	declarations: [
@@ -31,18 +32,9 @@ export class LoginComponent implements OnInit {
 	//DA SISTEMARE
 	errorMessage = "";
 	hide = true;
-	url: string;
 	debug = true;
 
 	constructor(private router: Router) {
-		//Ottengo i parametri URL dal file .ts
-		const ip = Globals.VIR2EMAPI_IP;
-		const port = Globals.VIR2EMAPI_PORT;
-		const protocol = Globals.VIR2EMAPI_PROTOCOL;
-
-		//Costruisco l'url
-		this.url = `${protocol}://${ip}:${port}`;
-
 		//Router per il rerouting della pagina.
 		this.router = router;
 	}
@@ -53,7 +45,7 @@ export class LoginComponent implements OnInit {
 		//console.log("Parsing\n");
 
 		function hash(p: string) {
-			return crypto.SHA256(p);
+			return crypto.SHA256(p).toString(crypto.enc.hex);
 		}
 		const username = ((document.getElementById("username"))as HTMLInputElement)?.value;
 		const password = ((document.getElementById("password"))as HTMLInputElement)?.value;
@@ -62,47 +54,42 @@ export class LoginComponent implements OnInit {
 			this.setErrorMessage("No data found");
 		}
 		else {
+			
 
 			//Creo il payload
 			const body = {
 				username: hash(username),
 				password: hash(password)
 			}
-
+			console.log("U,P=",	body.username, body.password);
+			//Setto l'header per contenuto della risposta.
+			const headers = new Headers()
+			headers.set("Content-Type", "application/json");
+			//headers.set("Origin", "http://localhost:4200");
+			//headers.set("Access-Control-Request-Method", "POST");
+			//headers.set("Access-Control-Request-Headers", "Content-Type, Origin, Access-Control-Request-Method")
+			
 			//Body e header della richiesta
 			let request_options: RequestInit = {
 				method: "POST",
 				body: JSON.stringify(body),
+				headers : headers,
 				redirect: "follow"
 			};
 
 			//Esecuzione e passaggio della callback per salvare il body
-			const token_response = fetch(this.url + '/login', request_options);
+			const token_response = fetch(Globals.getUrl()+ '/login', request_options);
 			token_response
 				.then(
 					//Richiesta ritorna 200, ritorno il token e lo mantengo.
 					(success: Response) => {
-						success
-							.text() // Brutto da morire ma TS mi costringe
-							.then(
-								(token) => {
-									if(token != undefined)
-									{	
-										console.log(token);
-										localStorage.setItem("access-token", token); //Storing del token
-									}
-									this.router.navigate(["/dashboard/table"]);
-								}
-							);
+						if(success.status == 200)
+							this.router.navigate(["/dashboard/table"]);
 					},
-					(failure: Response) => {
+					(failure) => {
 						//Ritornato errore. Invio messaggio di errore al frontend.
 						//Setto errtxt per side effect. 
-						let errtxt : string = "";
-						failure.text().then(function(error){
-							errtxt = error;
-						})
-						this.setErrorMessage(errtxt);
+						console.warn("Bad request:",failure);
 					}
 				)
 				.catch(
--- Frontend/src/globals.ts
+++ Frontend/src/globals.ts
@@ -3,12 +3,17 @@
 const Globals = {
     VIR2EMAPI_IP: "157.138.24.164",
     VIR2EMAPI_PROTOCOL: "http",
-    VIR2EMAPI_PORT: 9000
+    VIR2EMAPI_PORT: 9000,
     /*
     VIR2EMAPI_IP: "localhost",
     VIR2EMAPI_PROTOCOL: "http",
-    VIR2EMAPI_PORT: 5000
+    VIR2EMAPI_PORT: 8000,
     */
+
+    getUrl : ()=>{
+        return `${Globals.VIR2EMAPI_PROTOCOL}://${Globals.VIR2EMAPI_IP}:${Globals.VIR2EMAPI_PORT.toString()}`;
+    }
+
 }
 
 export { Globals };
