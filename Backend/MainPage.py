d7b55b494ce5bc04de2ba5820e0065eba0c9b138
FabioDanesin <f.danesin99@gmail.com>
2022-08-22 14:03:40 +0200

    file transfer

--- Backend/MainPage.py
+++ Backend/MainPage.py
@@ -526,7 +526,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return redirect(url_for(request.referrer))
+    return response
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
